2eeb6ba8f16b0fbbc2ad7a3c1294479c4fb04577
Nate <7nater.nelson@gmail.com>
2021-04-12 13:56:17 -0600

    commit tree 9310325e2dc3be8636a8b9765961aecb3a3194df

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const target = document.getElementById('text');
+    target.innerHTML = "This is really cool!";
+});
